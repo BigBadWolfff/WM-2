b609e6db01a59894f455d862739503ab5ba77d86
BigBadWolf <37703857+BigBadWolfff@users.noreply.github.com>
2024-03-15 17:18:42 +0100

    Add files via upload

--- js/movie-database.js
+++ js/movie-database.js
@@ -5,17 +5,18 @@ var movie = {};
 movies.push(movie);
 
 // film 1
-movie = {  
+movie = {
   Title: "1. Dune: Part Two",
   omschrijving: `</p>
   Dune: Part Two - De film
   Dune: Part Two is de langverwachte sequel op de epische sciencefictionfilm Dune uit 2021. De film, geregisseerd door Denis Villeneuve, vervolgt het verhaal van Paul Atreides (Timothée Chalamet) terwijl hij zijn lot omarmt als de Kwisatz Haderach, de langverwachte messias die de Fremen zal leiden in hun strijd tegen de Harkonnens en het Imperium.</p>
   <p>In deze film zien we Paul samenwerken met Chani (Zendaya) en de Fremen om Arrakis te bevrijden van de tirannieke Harkonnens. Paul moet zijn nieuw gevonden krachten en zijn politieke leiderschap inzetten om een interstellaire oorlog te voorkomen en tegelijkertijd zijn geliefden en zijn volk te beschermen. Dune: Part Two belooft een visueel spectaculaire en emotioneel beladen film te worden die fans van de Dune-franchise en sciencefictionliefhebbers in het algemeen zal boeien.</p>`,
-  trailerLink: "https://www.imdb.com/title/tt15239678/?ref_=nv_sr_srsg_3_tt_7_nm_1_q_dune%2520",
+  trailerLink:
+    "https://www.imdb.com/title/tt15239678/?ref_=nv_sr_srsg_3_tt_7_nm_1_q_dune%2520",
   coverImage: "../images/dune.webp",
   regiseur: "Denis Villeneuve",
   schrijvers: "Dennis Villeneuve, Jon Spaints, Frank Herbert",
-  acteurs: "Timothee Chamalet, Zendaya, Rebecca Ferguson",
+  acteurs: "Timothee Chamalet, Zendaya, Rebecca Ferguson"
 };
 movies.push(movie);
 
@@ -28,7 +29,7 @@ movie = {
   coverImage: "../images/one_love.jpg",
   regiseur: "Reinaldo Marcus Green",
   schrijvers: "Terence Winter, Frank E. Flowers, Zach Baylin",
-  acteurs: "Kinsley Ben-Adir, Lashana Lynch, James Norton",
+  acteurs: "Kinsley Ben-Adir, Lashana Lynch, James Norton"
 };
 movies.push(movie);
 
@@ -41,7 +42,7 @@ movie = {
   coverImage: "../images/poor_thing_s.jpg",
   regiseur: "Yorgos Lanthimos",
   schrijvers: "Tony McNamara, Alasdair Gray",
-  acteurs: "Emma Stone, Mark Ruffalo, Willem Dafoe",
+  acteurs: "Emma Stone, Mark Ruffalo, Willem Dafoe"
 };
 movies.push(movie);
 
@@ -51,10 +52,10 @@ movie = {
   omschrijving: `<p>De langverwachte derde film in de zeer populaire Verliefd Op-serie van romkom koning Johan Nijenhuis met een sterrencast met o.a. Anouk Maas, Jim Bakkum, Edwin Jonker en Soy Kroon.
   </p><p>Drie vrouwen, drie generaties, één eiland … een eiland om verliefd (op) te worden! Na het overlijden van haar Indische oma, overtuigt influencer Jenny haar moeder en zus om met haar naar Bali te gaan. Ze zijn totaal verschillend en komen ieder met hun eigen motivatie en vragen naar het eiland. Terwijl Jenny haar carrière probeert te redden, wil Dewi vooral op zoek naar haar roots en hoopt Cornelie dat de reis haar en haar dochters dichter bij elkaar brengt. Door de zoektocht naar een familielegende ontvouwt zich te midden van de kleurrijke Indonesische cultuur niet alleen een oude liefdesgeschiedenis, maar ontstaan er onverwacht ook nieuwe liefdes.</p>`,
   trailerLink: "https://www.imdb.com/title/tt27651263/?ref_=fn_al_tt_1",
-  coverImage: "../images/verliefd_op_bali_s.jpg",
+  coverImage: "../images/verliefd_op_bali.webp",
   regiseur: "Johan Nijenhuis",
   schrijvers: "Sander de Regt, Annelouise van Naerssen",
-  acteurs: "Anouk Maas, Jim Bakkum, Edwin Jonker",
+  acteurs: "Anouk Maas, Jim Bakkum, Edwin Jonker"
 };
 movies.push(movie);
 
@@ -63,10 +64,11 @@ movie = {
   Title: "5. Anyone But You",
   omschrijving: `<p>In de komedie Anyone But You lijken Bea (Sydney Sweeney) en Ben (Glen Powell) het perfecte stel, maar na een geweldige eerste date gebeurt er iets waardoor hun vurige aantrekkingskracht afkoelt tot het vriespunt – totdat ze onverwachts met elkaar worden opgezadeld op een bruiloft in Australië.
   </p><p>En wat doen twee volwassen mensen dan? Net doen alsof ze een stel zijn natuurlijk.</p>`,
-  trailerLink: "https://www.imdb.com/title/tt26047818/?ref_=nv_sr_srsg_0_tt_8_nm_0_q_anyone%2520but",
+  trailerLink:
+    "https://www.imdb.com/title/tt26047818/?ref_=nv_sr_srsg_0_tt_8_nm_0_q_anyone%2520but",
   coverImage: "../images/anyone_but_you.webp",
   regiseur: "Will Gluck",
   schrijvers: "Ilana Wolpert, Will Gluck",
-  acteurs: "Sydney Sweeney, Glen Powell, Alexandra Shipp",
+  acteurs: "Sydney Sweeney, Glen Powell, Alexandra Shipp"
 };
 movies.push(movie);
